e08454e1e4a7547e65580a01685b1bde255f3cda
Mojca <bozjamojca@gmail.com>
2019-06-06 10:52:55 +0200

    bookDetailsContoller

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,31 @@
+//BookDetails Controller
 /* prikaz vpisanih knjig v book details + shranjevanje */
+var bookDetailsContoller = (function() {
+
+    //library or wishlist
+    var library = function (title, author, year, isbn13, isbn10, sum, price, genre, file) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.isbn13 = isbn13;
+        this.isbn10 = isbn10;
+        this.sum = sum;
+        this.price = price;
+        this.genre = genre;
+        this.file = file;
+
+    };
+
+    //array for them
+
+    //saving data
+
+
+    
+}) ();
+
 /* izbris knjig */
+
 /* spreminjanje podatkov knjig */
+
 /* logiranje */
