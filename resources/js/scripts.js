a629934cdcda1ac2f981ed66456981c4a97bd68f
Mojca <bozjamojca@gmail.com>
2019-06-06 16:19:44 +0200

    Controller added ctrlAddBook

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -8,7 +8,21 @@ var detailsController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
 
-    //Some code
+    return {
+        getInput: function() {
+            return{
+                title: document.querySelector('.add_title').value,
+                author: document.querySelector('.add_author').value,
+                year: document.querySelector('.add_year').value,
+                isbn13: document.querySelector('.add_isbn13').value,
+                isbn10: document.querySelector('.add_isbn10').value,
+                summary: document.querySelector('.add_summary').value,
+                price: document.querySelector('.add_price').value,
+                genre: document.querySelector('.add_genre').value,
+                file: document.querySelector('.box__file').value
+            };
+        }
+    };
 
 }) ();
 
@@ -17,6 +31,9 @@ var controller = (function(detailsCtrl, UICtrl) {
 
     var ctrlAddBook = function() {
         // 1. Get the filed input data
+        var input = getInput();
+        console.log(input);
+
 
         // 2. Add a book to the details controller
 
