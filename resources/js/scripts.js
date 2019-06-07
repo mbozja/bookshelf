247ec62785af4df4794e1d6681ee9f23112bec2c
Mojca <bozjamojca@gmail.com>
2019-06-07 09:54:19 +0200

    Problem with input in controller

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -8,18 +8,30 @@ var detailsController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputTitle: '.add_title',
+        inputAuthor: '.add_author',
+        inputYear: '.add_year',
+        inputIsbn13: '.add_isbn13',
+        inputIsbn10: '.add_isbn10',
+        inputSummary: '.add_summary',
+        inputPrice: '.add_price',
+        inputGenre: '.add_genre',
+        inputFile: '.box__file'
+    };
+
     return {
         getInput: function() {
-            return{
-                title: document.querySelector('.add_title').value,
-                author: document.querySelector('.add_author').value,
-                year: document.querySelector('.add_year').value,
-                isbn13: document.querySelector('.add_isbn13').value,
-                isbn10: document.querySelector('.add_isbn10').value,
-                summary: document.querySelector('.add_summary').value,
-                price: document.querySelector('.add_price').value,
-                genre: document.querySelector('.add_genre').value,
-                file: document.querySelector('.box__file').value
+            return {
+                title: document.querySelector(DOMstrings.inputTitle).value,
+                author: document.querySelector(DOMstrings.inputAuthor).value,
+                year: document.querySelector(DOMstrings.inputYear).value,
+                isbn13: document.querySelector(DOMstrings.inputIsbn13).value,
+                isbn10: document.querySelector(DOMstrings.inputIsbn10).value,
+                summary: document.querySelector(DOMstrings.inputSummary).value,
+                price: document.querySelector(DOMstrings.inputPrice).value,
+                genre: document.querySelector(DOMstrings.inputGenre).value,
+                file: document.querySelector(DOMstrings.inputFile).value
             };
         }
     };
@@ -30,8 +42,9 @@ var UIController = (function() {
 var controller = (function(detailsCtrl, UICtrl) {
 
     var ctrlAddBook = function() {
+        //ze na zacetku :/ namest pol ko das input dejasnko notr
         // 1. Get the filed input data
-        var input = getInput();
+        var input = UICtrl.getInput();
         console.log(input);
 
 
@@ -42,15 +55,17 @@ var controller = (function(detailsCtrl, UICtrl) {
         // 4. Display a book on the UI
 
         //ze na začetku je, ne dela pravilno
-        console.log('It works!');
+        //console.log('It works!');
     }
 
-    document.querySelector('.add-btn').addEventListener('click', ctrlAddBook() );
+    document.getElementById("#add-btn").addEventListener("click", ctrlAddBook());
 
+    /* 
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 || event.which === 13) {
             ctrlAddBook();
         }
     });
+    */
 
 }) (detailsController, UIController);
