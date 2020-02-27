c16129133f4944d77de9907d4b608d1a747a2de7
mbozja <bozjamojca@gmail.com>
2020-02-27 11:51:09 +0100

    Added functions to open and close register form

--- src/index.js
+++ src/index.js
@@ -4,35 +4,59 @@ import Library from "./app/views/library/library";
 
 Library.init();
 
-/* FOR OPENING AND CLOSING FORM */
+/* ----------------------------------------- */
+// LOGIN POPUP
+/* ----------------------------------------- */
+/* FOR OPENING AND CLOSING LOGIN FORM */
 document
   .getElementById("button-open-login-popup")
-  .addEventListener("click", openForm);
-function openForm() {
+  .addEventListener("click", openLoginForm);
+function openLoginForm() {
   document.getElementById("popup-login").style.display = "block";
 }
 
 document
   .getElementById("button-close-login-popup")
-  .addEventListener("click", closeForm);
-function closeForm() {
+  .addEventListener("click", closeLoginForm);
+function closeLoginForm() {
   document.getElementById("popup-login").style.display = "none";
 }
 
 /* GET DATA FROM LOGIN FORM */
 const makeLogin = () => {
   // Get login form
-  var loginForm = document.getElementById("popup-login").firstElementChild;
-
   let loginFormElements = document.getElementById("formLogin").elements;
   console.log("loginFormElements:", loginFormElements);
+  // Get email and password
   let loginEmail = loginFormElements.namedItem("loginEmail").value;
   console.log("loginEmail:", loginEmail);
-  // Get email and password
+  let loginPass = loginFormElements.namedItem("loginPassword").value;
+  console.log("loginPass:", loginPass);
 };
+// To login
 document
   .getElementById("button-make-login")
   .addEventListener("click", makeLogin);
+
+/* ----------------------------------------- */
+// REGISTER POPUP
+/* ----------------------------------------- */
+/* FOR OPENING AND CLOSING REGISTER FORM */
+document
+  .getElementById("button-open-register-popup")
+  .addEventListener("click", openRegisterForm);
+function openRegisterForm() {
+  document.getElementById("popup-register").style.display = "block";
+  console.log("test 111111");
+}
+
+document
+  .getElementById("button-close-register-popup")
+  .addEventListener("click", closeRegisterForm);
+function closeRegisterForm() {
+  document.getElementById("popup-register").style.display = "none";
+  console.log("test 3333333");
+}
 
 let myVar = "Test text";
 let myVar_2 = "Test text 2";
